a0efc292a8ecef7f8b6b8b1ef8f27961ae322aa2
AsinoEsel <lerefff@gmail.com>
2025-12-28 12:02:10 +0100

    Fix bug where script would crash because of devices missing blackout and whiteout methods

--- userscripts/power_off_everything.py
+++ userscripts/power_off_everything.py
@@ -5,11 +5,17 @@ from controlpanel import api
 def power_off(event=None):
     print("PowerSwitch pressed: Initiating Blackout...")
     for fixture in api.Services.event_manager._fixture_dict.values():
-        fixture.blackout()
+        try:
+            fixture.blackout()
+        except AttributeError:
+            pass
 
 
 @api.callback(source="PowerSwitch", action="ButtonReleased")
 def power_on(event=None):
     print("PowerSwitch released: Restoring systems...")
     for fixture in api.Services.event_manager._fixture_dict.values():
-        fixture.whiteout()
+        try:
+            fixture.whiteout()
+        except AttributeError:
+            pass
