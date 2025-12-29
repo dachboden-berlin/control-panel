d877add8181c2f0b3aebe1caf49d065e0dde0060
AsinoEsel <lerefff@gmail.com>
2025-12-29 14:17:23 +0100

    Add terminal GUI script

--- userscripts/terminal_gui/__init__.py
+++ userscripts/terminal_gui/__init__.py
@@ -1,4 +1,31 @@
 from controlpanel import api
 from .main import TerminalGUI
+from datetime import datetime
 
-api.add_game(TerminalGUI(), True)
+
+terminal_gui = TerminalGUI()
+
+
+@api.callback(source="TerminalInputBox", action="TextInput")
+def console(event: api.Event[str]):
+    split = event.value.split(" ")
+    command, *args = split
+    if not command.startswith("/"):
+        time_str = datetime.now().strftime("%H:%M")
+        terminal_gui.desktop.elements[0].elements[0].print_to_log(f"{time_str} {event.value}")
+        return
+    match command:
+        case "/display":
+            api.get_device("fourteensegment").text = " ".join(args)
+        case _:
+            terminal_gui.desktop.elements[0].elements[0].print_to_log(f"Unknown command: {command}", (255, 255, 0))
+
+
+
+@api.call_with_frequency(4)
+def rotate_controlpanel_model():
+    terminal_gui.desktop.widget_manifest["ControlPanelAnim"].camera.rotate_left_right(5)
+    terminal_gui.desktop.widget_manifest["ControlPanelAnim"].flag_as_needing_rerender()
+
+
+api.add_game(terminal_gui, True)
--- userscripts/terminal_gui/main.py
+++ userscripts/terminal_gui/main.py
@@ -1,9 +1,49 @@
-from gui import WindowManager
-from .lasergame import LaserGame
+from gui import WindowManager, widgets
+
+
+GUI_GAP = 8
 
 
 class TerminalGUI(WindowManager):
     def __init__(self):
         super().__init__('TerminalGUI')
-        self.create_new_desktop("LaserGame", make_selected=True)
-        self.desktop.add_element(LaserGame("LaserGame", self.desktop), True)
+        width = self.screen.get_width()
+        height = self.screen.get_height()
+        self.create_new_desktop("GUI", make_selected=True)
+        self.desktop.add_element(widgets.Terminal("Terminal", self.desktop, x=GUI_GAP, y=GUI_GAP, w=width // 2 - GUI_GAP, h=height - 2 * GUI_GAP))
+        self.desktop.add_element(stl:=widgets.STLRenderer("ControlPanelAnim", self.desktop, "controlpanel.stl", x=width // 2 + GUI_GAP//2, y=GUI_GAP, w=width//2 - GUI_GAP, h=height - 2 * GUI_GAP))
+        stl.camera.rotate_up_down(30)
+        stl.camera.zoom *= 0.75
+        return
+        # desktop.add_element(
+        #     terminal :=
+        # desktop.add_element(
+        #     log := Log(desktop, x=RENDER_WIDTH // 2 + GUI_GAP, y=GUI_GAP, w=RENDER_WIDTH // 2 - 2 * GUI_GAP,
+        #                h=RENDER_HEIGHT // 2 - 2 * GUI_GAP))
+        # empty_widget = Widget(desktop, x=RENDER_WIDTH // 2 + GUI_GAP, y=RENDER_HEIGHT // 2 + GUI_GAP,
+        #                       w=RENDER_WIDTH // 2 - 2 * GUI_GAP, h=RENDER_HEIGHT // 2 - 2 * GUI_GAP)
+        # image = Image(desktop, x=RENDER_WIDTH // 2 + GUI_GAP, y=RENDER_HEIGHT // 2 + GUI_GAP,
+        #               w=RENDER_WIDTH // 2 - 2 * GUI_GAP, h=RENDER_HEIGHT // 2 - 2 * GUI_GAP,
+        #               image_path=os.path.join('media', 'robot36.png'))
+        # text_field = TextField(desktop, x=RENDER_WIDTH // 2 + GUI_GAP, y=RENDER_HEIGHT // 2 + GUI_GAP,
+        #                        w=RENDER_WIDTH // 2 - 2 * GUI_GAP, h=RENDER_HEIGHT // 2 - 2 * GUI_GAP,
+        #                        text=os.path.join('media', 'roboter_ascii.txt'), load_ascii_file=True, transparent=False,
+        #                        font=SMALL_FONT)
+        # desktop.add_element(STLRenderer(desktop, "media/fox_centered.stl", x=RENDER_WIDTH // 2 + GUI_GAP,
+        #                                 y=RENDER_HEIGHT // 2 + GUI_GAP,
+        #                                 w=RENDER_WIDTH // 2 - 2 * GUI_GAP, h=RENDER_HEIGHT // 2 - 2 * GUI_GAP))
+        # desktop.add_element(LoginWindow(desktop))
+        # desktop.terminal = terminal
+        # desktop.add_element(Taskbar(desktop, 20))
+        # log.print_to_log("ROTER TEXT", (255, 0, 0))
+        #
+        # desktop2 = desktops[1]
+        # desktop2.add_element(Radar(desktop2, png='media/red_dot_image.png'))
+        #
+        # desktop3 = desktops[2]
+        # from laser_game import LaserGame
+        # desktop3.add_element(LaserGame(desktop3))
+        #
+        # desktop4 = desktops[3]
+        # from dmx_monitor import DMXMonitor
+        # desktop4.add_element(DMXMonitor(None, 0, 0, RENDER_WIDTH, RENDER_HEIGHT))
